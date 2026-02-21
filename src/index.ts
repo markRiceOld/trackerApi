9cff076590b7dc405b98475dd5c4db4a1db3453a
TheMonster1995 <salmanian.foad2@gmail.com>
2026-02-21 08:11:28 +0330

    feat: cors added

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,17 @@ import { typeDefs, resolvers } from "./graphql";
 import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
 import { env } from "process";
+import cors from "cors";
 
 const prisma = new PrismaClient();
 const app = express();
+
+app.use(
+  cors({
+    origin: ["https://tracker.m-root.com"],
+    credentials: true,
+  })
+);
 
 const getUserFromToken = (req: any) => {
   const JWT_SECRET = env.JWT_SECRET || "dev-secret";
